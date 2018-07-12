2f123439e977ffcea7e8c6db139e74921df11086
someone <someone@someones-MacBook-Pro.local>
2018-07-12 19:48:33 +0630

    [modifiy] interactive event web3

--- sourcecode/solidity/FirstSolidity.sol
+++ sourcecode/solidity/FirstSolidity.sol
@@ -5,10 +5,16 @@ contract FirstSolidity {
    string positionName;
    uint age;
 
+    event Developer(
+        string name,
+        string position,
+        uint age
+        );
    function setDeveloper(string _firstName,string _positionName, uint _age) public {
        firstName = _firstName;
        positionName = _positionName;
        age = _age;
+       Developer(_firstName, _positionName, _age);
    }
    
    function getDeveloper() view public returns (string, string, uint) {
